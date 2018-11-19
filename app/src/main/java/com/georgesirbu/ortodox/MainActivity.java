da725c7d9b62127f11583d01df3436366dd3d277
George Sirbu <georgesirbu@outlook.it>
2018-11-19 22:44:13 +0100

    Corectat player click de pe lista si mesaj incarcare cand bufferul e in loading

--- app/src/main/java/com/georgesirbu/ortodox/MainActivity.java
+++ app/src/main/java/com/georgesirbu/ortodox/MainActivity.java
@@ -93,46 +93,46 @@ public class MainActivity extends AppCompatActivity {
         mContext = getApplicationContext();
         mActivity = MainActivity.this;
 
+        final ProgressDialog progressDialog =  ProgressDialog.show(MainActivity.this,
+                "Loading", "Loading Message");
 
+        progressDialog.dismiss();
 
         //if (isNetworkConnected()) {
-            ReadFileTask tsk = new ReadFileTask();
-            tsk.execute("https://georgesirbu.com/Ortodox/liste/media.lst");
+        ReadFileTask tsk = new ReadFileTask();
+        tsk.execute("https://georgesirbu.com/Ortodox/liste/media.lst");
 
-            while(fineHTTP==0) {
-                Log.d("LISTA: ", listaMedia);
+        while(fineHTTP==0) {
+            Log.d("LISTA: ", listaMedia);
+        }
+
+        String[] parts = listaMedia.split(">");
+        int size = parts.length;
+        String[] data = new String[size/2];
+        final String[] links = new String[size/2];
+        int n = 0;
+        int l = 0;
+
+        for (int i=0; i<size; i++) {
+
+            int p = 2;
+
+            int resto = i % p;
+
+            if (resto == 0) {
+                data[n] = parts[i];
+                n++;
+            } else {
+                links[l] = parts[i];
+                l++;
             }
 
-            String[] parts = listaMedia.split(">");
-            int size = parts.length;
-            String[] data = new String[size/2];
-            final String[] links = new String[size/2];
-            int n = 0;
-            int l = 0;
+        }
 
-            for (int i=0; i<size; i++) {
+        //String[] data=new String[]{"Torino","Roma","Milano","Napoli","Firenze"};
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, R.layout.single_row, R.id.textView, data);
 
-                int p = 2;
-
-                int resto = i % p;
-
-                if (resto == 0) {
-                    data[n] = parts[i];
-                    n++;
-                } else {
-                    links[l] = parts[i];
-                    l++;
-                }
-
-            }
-
-            //String[] data=new String[]{"Torino","Roma","Milano","Napoli","Firenze"};
-            ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, R.layout.single_row, R.id.textView, data);
-
-            listView.setAdapter(adapter);
-
-
-
+        listView.setAdapter(adapter);
 
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -141,15 +141,27 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
                 // TODO Auto-generated method stub
 
-
-                if (selectedLink != "")
-                {
-
+                try {
                     mPlayer.stop();
-
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                selectedLink = links[+position];
 
+               if (mPlayer != null) {
+                   mPlayer.reset();
+                   mPlayer.release();
+                   mPlayer = null;
+               }else
+               {
+
+               }
+
+
+                mButtonPlay.setImageResource(R.drawable.playarrow);
+                played = false;
+
+
+                selectedLink = links[+position];
                 mButtonPlay.performClick();
 
             }
@@ -159,131 +171,94 @@ public class MainActivity extends AppCompatActivity {
         mButtonPlay.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                // click handling code
 
-               // loadingDialog.show();
-
-                // Disable the play button
-                //mButtonPlay.setEnabled(false);
-               if(played == false ) {
+                if(played == false ) {
 
 
-                   mButtonPlay.setImageResource(R.drawable.pausebutton);
+                    progressDialog.show();
 
-                // The audio url to play
-                String audioUrl = selectedLink;
+                    // Initialize a new media player instance
+                    mPlayer = new MediaPlayer();
 
-                // Initialize a new media player instance
-                mPlayer = new MediaPlayer();
+                    mButtonPlay.setImageResource(R.drawable.pausebutton);
 
-                // Set the media player audio stream type
-                mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                //Try to play music/audio from url
-
-                try{
-                    /*
-                        void setDataSource (String path)
-                            Sets the data source (file-path or http/rtsp URL) to use.
-
-                        Parameters
-                            path String : the path of the file, or the http/rtsp URL of the stream you want to play
-
-                        Throws
-                            IllegalStateException : if it is called in an invalid state
-
-                                When path refers to a local file, the file may actually be opened by a
-                                process other than the calling application. This implies that the
-                                pathname should be an absolute path (as any other process runs with
-                                unspecified current working directory), and that the pathname should
-                                reference a world-readable file. As an alternative, the application
-                                could first open the file for reading, and then use the file
-                                descriptor form setDataSource(FileDescriptor).
-
-                            IOException
-                            IllegalArgumentException
-                            SecurityException
-                    */
-                    // Set the audio data source
-                    mPlayer.setDataSource(audioUrl);
-
-                    /*
-                        void prepare ()
-                            Prepares the player for playback, synchronously. After setting the
-                            datasource and the display surface, you need to either call prepare()
-                            or prepareAsync(). For files, it is OK to call prepare(), which blocks
-                            until MediaPlayer is ready for playback.
-
-                        Throws
-                            IllegalStateException : if it is called in an invalid state
-                            IOException
-                    */
-                    // Prepare the media player
-                    mPlayer.prepare();
-
-                    // Start playing audio from http url
-                    mPlayer.start();
-
-                    played = true;
-
-                    // Inform user for audio streaming
-                    //Toast.makeText(mContext,"Playing",Toast.LENGTH_SHORT).show();
+                    // The audio url to play
+                    String audioUrl = selectedLink;
 
 
-                }catch (IOException e){
-                    // Catch the exception
-                    e.printStackTrace();
-                    //loadingDialog.dismiss();
-                }catch (IllegalArgumentException e){
-                    e.printStackTrace();
-                    //loadingDialog.dismiss();
-                }catch (SecurityException e){
-                    e.printStackTrace();
-                    //loadingDialog.dismiss();
-                }catch (IllegalStateException e){
-                    e.printStackTrace();
-                    //loadingDialog.dismiss();
-                }
 
-                mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        Toast.makeText(mContext,"End",Toast.LENGTH_SHORT).show();
-                        mButtonPlay.setImageResource(R.drawable.playarrow);
+                    // Set the media player audio stream type
+                    mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    //Try to play music/audio from url
+
+                    try{
+
+
+                        // Set the audio data source
+                        mPlayer.setDataSource(audioUrl);
+
+
+                        // Prepare the media player
+                        mPlayer.prepare();
+
+
+
+                        // Start playing audio from http url
+                        mPlayer.start();
+
                         played = true;
-                    }
-                });
-                mPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
 
-                    public void onBufferingUpdate(MediaPlayer mp, int percent)
-                    {
+
+
+                        // Inform user for audio streaming
+                        //Toast.makeText(mContext,"Playing",Toast.LENGTH_SHORT).show();
+
+
+                    }catch (IOException e){
+                        // Catch the exception
+                        e.printStackTrace();
+                        //loadingDialog.dismiss();
+                    }catch (IllegalArgumentException e){
+                        e.printStackTrace();
+                        //loadingDialog.dismiss();
+                    }catch (SecurityException e){
+                        e.printStackTrace();
+                        //loadingDialog.dismiss();
+                    }catch (IllegalStateException e){
+                        e.printStackTrace();
                         //loadingDialog.dismiss();
                     }
 
-                });
+
+                    mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mediaPlayer) {
+                            Toast.makeText(mContext,"End",Toast.LENGTH_SHORT).show();
+                            mButtonPlay.setImageResource(R.drawable.playarrow);
+                            played = false;
+                        }
+                    });
 
 
-                mPlayer.setOnInfoListener(new MediaPlayer.OnInfoListener() {
-                    @Override
-                    public boolean onInfo(MediaPlayer mp, int what, int extra) {
 
-                        if (what == MediaPlayer.MEDIA_INFO_BUFFERING_START) {
-                           // loadingDialog.show();
-                        } else if (what == MediaPlayer.MEDIA_INFO_BUFFERING_END) {
-                            // dismiss progress dialog
-                            //loadingDialog.dismiss();
+                    mPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+
+                            if (progressDialog != null && progressDialog.isShowing()){
+                                progressDialog.dismiss();
+                            }
+
                         }
 
 
-                        return false;
-                    }
-                });
+                    });
 
 
 
 
 
-
-            }else
+                }else
                 {
                     mPlayer.stop();
                     mButtonPlay.setImageResource(R.drawable.playarrow);
@@ -293,12 +268,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
-
-
-    private boolean isNetworkConnected() {
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        return cm.getActiveNetworkInfo() != null;
     }
 
     private class ReadFileTask extends AsyncTask<String,Integer,String> {
